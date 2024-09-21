47048c0a641adb863a949b291c2a785f6622a02b
Cris-noscore <cristiano.silveira@fatec.sp.gov.br>
2024-09-21 16:46:51 -0300

    Solving routes problems; images, pages and fixing some css pages

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router= express.Router();
 
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
 router.get('/login', function(req,res){
     res.render('login');
 });
@@ -8,9 +12,35 @@ router.get('/login', function(req,res){
 router.get('/signin', function(req,res){
     res.render('SIGNIN');
 });
-router.get('/', function (req, res) {
-    res.render('index');
+
+router.get('/attAm', function(req,res){
+    res.render('attAM');
 });
+
+router.get('/attMusico', function(req,res){
+    res.render('attMUSICO');
+});
+
+router.get('/attProdutor', function(req,res){
+    res.render('attPRODUTOR');
+});
+
+router.get('/confirm', function(req,res){
+    res.render('confirm');
+});
+
+router.get('/portifolio-am', function(req,res){
+    res.render('portifolio-AM');
+});
+
+router.get('/portifolio-musico', function(req,res){
+    res.render('portifolio-musico');
+});
+
+router.get('/portifolio-produtor', function(req,res){
+    res.render('portifolio-produtor');
+});
+
 
 
 module.exports = router;
